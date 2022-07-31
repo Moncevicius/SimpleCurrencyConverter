503d566bd7fee845cc137d2857ba8457fc10f35e
Moncevicius <moncevicius@googlemail.com>
2022-07-31 15:16:24 +0300

    Update

--- Simple Currency Converter/task/main.js
+++ Simple Currency Converter/task/main.js
@@ -1,47 +1,89 @@
 let input = require('sync-input');
 let multiplierFrom = 0, multiplierTo = 0;
 let currencyFrom, currencyTo;
-let currency = ["USD", "JPY", "EUR", "RUB", "GBP"];
-let currencyRates = {
-  "USD": 1,
-  "JPY": 113.5,
-  "EUR": 0.89,
-  "RUB": 74.36,
-  "GBP": 0.75
-};
 
-console.log(`Welcome to Currency Converter!`);
-currency.forEach(n => {
-    console.log(`1 USD equals  ${currencyRates[n]} ${n}`);
-})
+console.log(`Welcome to Currency Converter!
+1 USD equals  1 USD
+1 USD equals  113.5 JPY
+1 USD equals  0.89 EUR
+1 USD equals  74.36 RUB
+1 USD equals  0.75 GBP`);
 console.log("What do you want to do?")
 
 while(true){
 
-    choice = Number(input("1-Convert currencies 2-Exit program\n"));
+    console.log("1-Convert currencies 2-Exit program");
+    choice = Number(input());
+
     switch(choice){
         case 1: {
-            console.log(`What do you want to convert?`);
-            while(true){
-                currencyFrom = input("From:").toUpperCase();
-                if(currency.includes(currencyFrom)){
-                    multiplierFrom = currencyRates[currencyFrom];
-                    break;
-                }else {
-                    console.log("Unknown currency");
+            console.log(`What do you want to convert?
+From:`);
+            let currencyFromLoop = 0;
+            while(currencyFromLoop === 0){
+                currencyFrom = input().toUpperCase();
+                switch (currencyFrom){
+                    case "USD":
+                        multiplierFrom = 1;
+                        currencyFromLoop = 1;
+                        break;
+                    case "JPY":
+                        multiplierFrom = 113.5;
+                        currencyFromLoop = 1;
+                        break;
+                    case "EUR":
+                        multiplierFrom = 0.89;
+                        currencyFromLoop = 1;
+                        break;
+                    case "RUB":
+                        multiplierFrom = 74.36;
+                        currencyFromLoop = 1;
+                        break;
+                    case "GBP":
+                        multiplierFrom = 0.75;
+                        currencyFromLoop = 1;
+                        break;
+                    default:
+                        console.log("Unknown currency");
+                        break;
                 }
             }
-            while(true){
-                currencyTo = input("To:").toUpperCase();
-                if(currency.includes(currencyTo)) {
-                    multiplierTo = currencyRates[currencyTo];
-                    break;
-                }else{
-                    console.log("Unknown currency");
+
+            console.log("To:")
+            let currencyToLoop = 0;
+            while(currencyToLoop === 0){
+                currencyTo = input().toUpperCase();
+                switch (currencyTo){
+                    case "USD":
+                        multiplierTo = 1;
+                        currencyToLoop = 1;
+                        break;
+                    case "JPY":
+                        multiplierTo = 113.5;
+                        currencyToLoop = 1;
+                        break;
+                    case "EUR":
+                        multiplierTo = 0.89;
+                        currencyToLoop = 1;
+                        break;
+                    case "RUB":
+                        multiplierTo = 74.36;
+                        currencyToLoop = 1;
+                        break;
+                    case "GBP":
+                        multiplierTo = 0.75;
+                        currencyToLoop = 1;
+                        break;
+                    default:
+                        console.log("Unknown currency");
+                        break;
                 }
             }
-            while(true){
-                let amount = Number(input("Amount:"));
+
+            let amountLoop = 0;
+            while(amountLoop === 0){
+                console.log("Amount:")
+                let amount = Number(input());
                 if(amount < 1){
                     console.log("The amount can not be less than 1");
                 }else if(isNaN(amount)){
@@ -49,17 +91,17 @@ while(true){
                 }else {
                     result = (amount / multiplierFrom) * multiplierTo;
                     console.log(`Result: ${amount} ${currencyFrom} equals ${result.toFixed(4)} ${currencyTo}`);
-                    break;
+                    amountLoop = 1;
                 }
             }
             break;
+
         }
         case 2:
             console.log("Have a nice day!");
             return;
         default:
-            console.log("Unknown input");
+            //"Unknown input";
             break;
     }
-
 }
